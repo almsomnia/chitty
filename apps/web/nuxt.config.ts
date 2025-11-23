830b488303fcccb4d0e1aaf295faf098767138bc
Riva Almero <rivaalms@proton.me>
2025-11-23 14:34:16 +0700

    chore: setup web app

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+   compatibilityDate: "2025-07-15",
+   devtools: { enabled: true },
+   ssr: false,
+   modules: ["@nuxt/ui", "@pinia/nuxt", "@vueuse/nuxt"],
+   css: ["~/assets/css/main.css"],
+   components: {
+      dirs: [
+         {
+            path: "~/components",
+            pathPrefix: false,
+         },
+      ],
+   },
 })
